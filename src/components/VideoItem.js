78994dfafc1135c39896f8c65591d90ed6d6bdf7
marikomands <marikomands@gmail.com>
2022-01-21 14:24:54 +0000

    addee alt for img tag

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -5,7 +5,11 @@ const VideoItem = ({ video, onVideoSelect }) => {
   console.log("🚀 ~ VideoItem ~ video", video);
   return (
     <div onClick={() => onVideoSelect(video)} className="video-item item">
-      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt={video.snippet.title}
+      />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
       </div>
