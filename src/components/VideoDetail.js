bbca5de07126ec18fab41f6b6d632a41728467f6
marikomands <marikomands@gmail.com>
2022-01-26 11:11:57 +0000

    added iframe for a selected video

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,14 +1,16 @@
 import React from "react";
+// import "./VideoDetail.css";
 
 const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>Loading...</div>;
   }
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
     <div>
       <div className="ui embeded">
-        <iframe title="video player" src={videoSrc} />
+        <iframe className="img" title="video player" src={videoSrc} />
       </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
